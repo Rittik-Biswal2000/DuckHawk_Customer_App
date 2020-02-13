c507edb96eb606f47d61e4fcd47775a190eb853d
aitik <aitikdandapat@gmail.com>
2020-02-14 02:20:19 +0530

    added appbar + drawer

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,34 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage()
     )
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Text('hey')
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Color(104670), //CHECK COLOR CODE
+        title: Text('Your Location \nBURLA'),
+      ),
+      endDrawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(accountName: Text('Ekanta'), accountEmail: null,currentAccountPicture: GestureDetector(
+              child: new CircleAvatar(),),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
