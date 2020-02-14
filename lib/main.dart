13f3113d7c40da7ac06239cfa87a1f922d06c295
Rittik-Biswal2000 <47083480+Rittik-Biswal2000@users.noreply.github.com>
2020-02-15 00:17:44 +0530

    Added product grid ,+ footer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:project_duckhawk/components/horizontal_listview.dart';
+import 'package:project_duckhawk/components/products.dart';
 
 void main(){
   runApp(
@@ -16,8 +18,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _value = '';
+  void _onClick(String value) => setState(() => _value = value);
   @override
   Widget build(BuildContext context) {
+    Widget image_carousel=new Container(
+      height:200.0,
+      child:new Carousel(
+        boxFit: BoxFit.cover,
+        images:[
+          AssetImage('images/armani.png')
+
+
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+      ),
+    );
+  Widget image_carousel1=new Container(
+    height:200.00,
+    child:new Carousel(
+      boxFit: BoxFit.cover,
+      images:[
+        AssetImage('images/armani.png'),
+        AssetImage('images/watches-111a.png'),
+        AssetImage('images/Guide-mens-smart-casual-dress-code15@2x.png'),
+        AssetImage('images/men-jeans@2x.png'),
+
+      ],
+      autoplay: true,
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: Duration(milliseconds: 1000),
+    ),
+  );
+
     return Scaffold(
       appBar: new AppBar(
 
@@ -25,6 +60,14 @@ class _HomePageState extends State<HomePage> {
         title: Text('Your Location \nBURLA'),
 
       ),
+      persistentFooterButtons: <Widget>[
+        new IconButton(icon: new Icon(Icons.add_shopping_cart), onPressed: () => _onClick('Button1')),
+        new IconButton(icon: new Icon(Icons.shop), onPressed: () => _onClick('Button2')),
+        new IconButton(icon: new Icon(Icons.monetization_on), onPressed: () => _onClick('Button3')),
+        new IconButton(icon: new Icon(Icons.notifications), onPressed: () => _onClick('Button4')),
+        new IconButton(icon: new Icon(Icons.share), onPressed: () => _onClick('Button5')),
+      ],
+
       endDrawer: new Drawer(
         child: new ListView(
           children: <Widget>[
@@ -41,7 +84,8 @@ class _HomePageState extends State<HomePage> {
             InkWell(
               onTap: (){},
               child: ListTile(
-                title: Text('Men')
+
+                title: Text('Men'),
               ),
             ),
 
@@ -88,6 +132,7 @@ class _HomePageState extends State<HomePage> {
             ),
 
             Divider(),
+
 
             InkWell(
               onTap: (){},
@@ -156,14 +201,35 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-
-      body: new ListView(
+      body:new ListView(
         children: <Widget>[
           new Padding(padding: const EdgeInsets.all(2.0),
-          child: new Text('Categories'),),
-          HorizontalList(),
+            child: new Text('Categories'),),
+            HorizontalList(),
+          new Padding(padding: const EdgeInsets.all(8.0)),
+          image_carousel,
+          image_carousel1,
+          new Padding(padding: const EdgeInsets.all(20.0),
+            child:new Text('Fashion'
+                ) ,),
+
+
+          Container(
+            height: 320.0,
+            child: products(),
+          ),
+          new Padding(padding: const EdgeInsets.all(20.0),
+            child:new Text('Electronics'
+            ) ,),
+
+          Container(
+            height:320.0,
+            child:products(),
+
+          ),
         ],
-      )
+      ),
+
     );
   }
 }
