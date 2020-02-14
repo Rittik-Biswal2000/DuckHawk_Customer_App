e892269697c21aee08990ae03556cfb65a9bb08c
aitik <aitikdandapat@gmail.com>
2020-02-14 14:07:12 +0530

    added appbar + drawer

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,139 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        backgroundColor: Color(104670), //CHECK COLOR CODE
+
+        backgroundColor: Color(0xff104670), //CHECK COLOR CODE
         title: Text('Your Location \nBURLA'),
+
       ),
       endDrawer: new Drawer(
         child: new ListView(
           children: <Widget>[
             new UserAccountsDrawerHeader(accountName: Text('Ekanta'), accountEmail: null,currentAccountPicture: GestureDetector(
-              child: new CircleAvatar(),),
+              child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                )
+              ),
+              decoration: new BoxDecoration(
+                color: Color(0xff104670)
+              ),
             ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text('Men')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Women')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Electronics')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Sports')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Books')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Home & Furniture')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Beauty & Personal Care')
+              ),
+            ),
+
+            Divider(),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('My Orders')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('My Cart')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Account')
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Notifications')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Budget')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Share')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('Settings')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('LOGOUT')
+              ),
+            ),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                  title: Text('HELP')
+              ),
+            ),
+
+
+
           ],
         ),
       ),
