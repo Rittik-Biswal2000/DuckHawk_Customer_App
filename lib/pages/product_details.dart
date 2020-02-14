d3264328bed41c7153ecce24adbd5944a288d7d3
AITIK DANDAPAT <46849982+i-tick@users.noreply.github.com>
2020-02-15 02:58:39 +0530

    Add files via upload

---
+++ lib/pages/product_details.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ProductDetails extends StatefulWidget {
+  @override
+  _ProductDetailsState createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+
+           //CHECK COLOR CODE
+           title: Text('Your Location \nBURLA'),
+
+      ),
+
+    );
+  }
+}
